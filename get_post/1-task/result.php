29813dbe50473493e19495815da9f6aab71b3daa
shohNorkuziev <113886924+shohNorkuziev@users.noreply.github.com>
2023-04-29 14:06:28 +0500

    Delete get_post directory

--- get_post/1-task/result.php
+++
@@ -1,8 +0,0 @@
-
-    Здравствуйте, <?php echo htmlspecialchars($_GET['name']); ?>.
-    <br>
-    Ваше сообщение: <?php echo htmlspecialchars($_GET['messages']); ?>.
-    <br>
-    Ваш выбор: <?php echo htmlspecialchars($_GET['sha_den']); ?>.
-    <br>
-    Ваш любимый язык программирования: <?php echo htmlspecialchars($_GET['language']); ?>.
